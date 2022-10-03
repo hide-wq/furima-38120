f5186036a47c0b3c09e93b51f69fc5eaddb1d7a8
hide-wq <t9un.sora@gmail.com>
2022-10-04 03:59:42 +0900

    新規登録のバリデーション
    ・ユーザー情報
    ・本人情報確認
    上記２つのバリデーションを記述

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  
+  validates :nickname, presence: true
+   validates :email, presence: true
+
+   # パスワードは６文字から登録できる
+   validates :encrypted_password, presence: true,length: { minimum: 6 }
+  
+   # 全角文字のバリデーション
+  
+   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  # 全角カナのバリデーション
+  
+  with_options presence: true, format: { with: /\A[\p{katakana}]+\z/, message: '全角カナを使用してください' } do
+   validates :last_name_kana, presence: true
+   validates :first_name_kana, presence: true
+  end
+   validates :birthday, presence: true
+   
+  # アソシエーション
+   has_many :items
+   has_many :orders
+
+end
