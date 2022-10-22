5d0f34e145d6df8059fa4a387c897d952fdf5ef7
hide-wq <t9un.sora@gmail.com>
2022-10-22 18:41:39 +0900

    指摘点の修正
    math.floorを使い販売手数料と販売利益を小数点以下を切り捨てて表示すること。

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -4,7 +4,7 @@ function calculate (){
     let amount_number = Number(amount.value) 
     let fee = Math.floor(amount_number * 0.1)
     const fee_box = document.getElementById("add-tax-price")
-    let profit = amount_number - fee
+    let profit = Math.floor(amount_number - fee)
     const profit_box = document.getElementById("profit")
     fee_box.innerHTML = `${fee}`
     profit_box.innerHTML = `${profit}`
