46ef48caaee72705bff6faa47e5bb4d85354fc33
hide-wq <t9un.sora@gmail.com>
2022-11-02 23:37:44 +0900

    マイグレーションの変更に伴う修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,12 +1,12 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :block, :building, :phone_number
 
   # ここにバリデーションの処理を書く
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id, numericality: { other_than: 0}
     validates :city
     validates :block
@@ -15,6 +15,6 @@ class OrderForm
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Payment.create(order_id: order.id, postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number)
+    Payment.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number)
   end
 end
