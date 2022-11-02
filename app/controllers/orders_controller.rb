c97a543da7c2be68683e77c1f5ad336c328c5332
hide-wq <t9un.sora@gmail.com>
2022-11-03 01:26:15 +0900

    ビューとコントローラーの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,25 @@
 class OrdersController < ApplicationController
+
+  def index
+    @order_form = OrderForm.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order_form = OrderForm.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
